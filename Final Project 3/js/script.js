069ddfe08d03007bf0bbc5948719a68cb2ea665d
Aldo Vierisyah <109676907+AldoVierisyah@users.noreply.github.com>
2022-11-29 01:14:21 +0700

    Home

---
+++ Final Project 3/js/script.js
@@ -0,0 +1,12 @@
+  const viewBtn = document.querySelector(".popup_button"),
+    popup = document.querySelector(".popup"),
+    close = popup.querySelector(".close")
+  viewBtn.onclick = () => {
+    popup.classList.toggle("show");
+  }
+  const btn = document.querySelector(".btn_rate");
+  btn.onclick = () => {
+    editBtn.onclick = () => {
+    }
+    return false;
+  }
